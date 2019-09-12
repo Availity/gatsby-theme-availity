9fb66de759bb15303a2c100f386533af2ba04868
Kyle Gray <kgray1497@gmail.com>
2019-09-11 23:06:29 -0400

    refactor(gatsby-theme-core): added logic for seach to open when hitting slash

--- packages/gatsby-theme-core/src/components/TopNav/Search.js
+++ packages/gatsby-theme-core/src/components/TopNav/Search.js
@@ -1,6 +1,26 @@
 /* global docsearch */
 import React, { useEffect, useState, useRef } from 'react';
+import classnames from 'classnames';
+import { FaTimes } from 'react-icons/fa';
 import { Nav, Form, Input } from 'reactstrap';
+
+const HotKey = ({ className, style, ...props }) => (
+  <span
+    className={classnames(
+      className,
+      'rounded text-center py-1 px-2 position-absolute'
+    )}
+    style={{
+      ...style,
+      zIndex: 6,
+      right: 8,
+      lineHeight: 1.125,
+      top: '50%',
+      transform: 'translateY(-50%)',
+    }}
+    {...props}
+  />
+);
 
 // Overlay for the opacity overtop the search/results
 const Overlay = ({ visible }) => (
@@ -28,12 +48,14 @@ const Search = () => {
   const [focused, setFocused] = useState(false);
   const [value, setValue] = useState('');
   const input = useRef();
+  const search = useRef(null);
 
   const onKeyDown = event => {
     // focus the input when the slash key is pressed
     if (
       event.keyCode === 191 &&
-      event.target.tagName.toUpperCase() !== 'INPUT'
+      event.target.tagName.toUpperCase() !== 'INPUT' &&
+      event.target.tagName.toUpperCase() !== 'TEXTAREA'
     ) {
       event.preventDefault();
       input.current.focus();
@@ -43,7 +65,7 @@ const Search = () => {
   // On mount add event listeners for focusing and updating the input
   useEffect(() => {
     if (typeof docsearch !== 'undefined') {
-      docsearch({
+      search.current = docsearch({
         apiKey: 'eec0154a008662c32d440b7de7982cd2',
         indexName: 'availity',
         inputSelector: '#input',
@@ -65,6 +87,13 @@ const Search = () => {
 
   const onBlur = () => setFocused(false);
 
+  const reset = () => {
+    setValue('');
+    if (search.current) {
+      search.current.autocomplete.autocomplete.setVal('');
+    }
+  };
+
   const resultsShown = focused && value.trim();
 
   return (
@@ -72,9 +101,10 @@ const Search = () => {
       className="ml-5 d-xs-none d-sm-none d-md-none d-lg-none d-xl-block"
       navbar
     >
-      <Form inline>
+      <Form inline className="position-relative">
         <Overlay visible={resultsShown} />
         <Input
+          innerRef={input}
           onFocus={onFocus}
           onBlur={onBlur}
           onChange={onChange}
@@ -83,6 +113,25 @@ const Search = () => {
           placeholder="Search Availity Docs"
           style={{ zIndex: 5, width: 400 }}
         />
+        {focused && value && (
+          <HotKey
+            onClick={reset}
+            style={{
+              cursor: 'pointer',
+            }}
+            onMouseDown={event => event.preventDefault()}
+          >
+            <FaTimes />
+          </HotKey>
+        )}
+        {!focused && (
+          <HotKey
+            style={{ pointerEvents: 'none' }}
+            className="border border-light text-light"
+          >
+            /
+          </HotKey>
+        )}
         <Overlay />
       </Form>
     </Nav>
